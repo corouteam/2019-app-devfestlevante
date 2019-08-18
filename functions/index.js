2d1a66c67602dc12000e0c1d231ef70cc0f14f50
Domenico Rotolo <rotolonico@gmail.com>
2019-08-18 12:40:58 +0200

    Added notification cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,37 @@
-let functions = require('firebase-functions');
-let admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+const functions = require('firebase-functions');
 
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// exports.banana = functions.firestore
-//     .document('users/{userId}')
-//     .onWrite((change, context) => {
-//         console.log(`new create event for document ID: ${context.params.userId.notificationToken}`);
-//         var token = context.params.notificationToken;
+exports.sendNotification = functions.firestore.document('appNotifications/{id}')
+.onCreate((snapshot, context) => {
+    var notificationMessage = snapshot.data().message;
 
-//         const payload = {
-//             notification: {
-//                 title:"Hai aggiunto un preferito!",
-//                 body: "Figo!",
-//                 sound: "default"
-//             },
-//         };
+    const payload = {
+        notification: {
+            title: 'Devfest Levante 2019',
+            body: notificationMessage
+        }
+   };
 
-//         return admin.messaging().sendToDevice(token, payload)
+   return admin.messaging().sendToTopic("devfest_levante_2019", payload)
+});
 
+exports.createNotification = functions.firestore.document('createAppNotifications/{id}')
+.onCreate((snapshot, context) => {
 
-//     });
+    var notificationMessage = snapshot.data().message;
+    var notificationType = snapshot.data().type;
+    
+    var timestamp = (new Date()).getTime();
+    
+    admin.firestore().doc('appNotifications/' + timestamp).set({
+        message: notificationMessage,
+        timestamp: timestamp,
+        type: notificationType === null ? "generic" : notificationType
+    }).then(function (snapshot) {
+        return admin.firestore().doc('createAppNotifications/' + timestamp).delete();
+    }).catch(function (error) {
+        return console.log(error);
+    });
+});
